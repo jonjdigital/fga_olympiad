c98864173e1fd04b65a62dc7e95a8b06c39eee7f
JON-DANIEL FLETCHER-JAMES <jf286548@falmouth.ac.uk>
2022-09-15 11:49:03 +0100

    init

--- main.py
+++ main.py
@@ -18,11 +18,29 @@ motorBack = Motor(Port.A)
 motorLeft = Motor(Port.B)
 motorRight = Motor(Port.C)
 
-distanceSensor = UltrasonicSensor(Port.1)
+distanceSensor = UltrasonicSensor(Port.S1)
+touch = TouchSensor(Port.S2)
 
+driver = DriveBase(motorLeft,motorRight,80,40)
+
+# Write your program here.
+ev3.speaker.beep()
 
 # Write your program here.
 ev3.speaker.beep()
 
 while True:
-    print(distanceSensor)
+    # Begin driving forward at 200 millimeters per second.
+    driver.drive(-200, 0)
+
+    # Wait until an obstacle is detected. This is done by repeatedly
+    # doing nothing (waiting for 10 milliseconds) while the measured
+    # distance is still greater than 300 mm.
+    while distanceSensor.distance() > 300:
+        wait(10)
+
+    # Drive backward for 300 millimeters.
+    driver.straight(300)
+
+    # Turn around by 120 degrees
+    driver.turn(120)
